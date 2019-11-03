4a3856f80e9fedeee7a14844e1fe75304c86d576
btjanaka <bryon.tjanaka@gmail.com>
2019-11-03 11:05:59 -0800

    Save results from individual molecules to CSV
    The number of trivalent nitrogens and the SMILES for each molecule is
    now saved in a separate CSV, so that we do not have to go back and count
    again.
    
    Also, the function for counting trivalent nitrogens was modified - it
    should provide the same functionality though.

--- emolecules-counting/count_nitrogens.py
+++ emolecules-counting/count_nitrogens.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 # Outputs statistics about trivalent nitrogens in various molecules.
 #
-# Input: a list of directories, each directory containing mol2 files
-#        corresponding to the molecules
-# Output: a markdown file describing the number of molecules with 0 trivalent
-#         nitrogens, 1 trivalent nitrogen, 2, trivalent nitrogens,...
+# Input:
+#    - a list of directories, each directory containing mol2 files
+#      corresponding to the molecules
+# Output:
+#    - a CSV in the format {filepath,SMILES,num} listing the number of trivalent
+#      nitrogens in each molecule
+#    - a markdown file describing the number of molecules with 0 trivalent
+#      nitrogens, 1 trivalent nitrogen, 2, trivalent nitrogens,...
 #
 # Usage:
-#   count_nitrogens.py --dirs DIR1,DIR2,DIR3,... --output MD
+#   count_nitrogens.py --dirs DIR1,DIR2,DIR3,... --complete CSV --summary MD
 # Example:
-#   count_nitrogens.py --dirs ./1,./2,./3 --output results.md
-#     # Reads molecules in directories ./1, ./2, and ./3, and writes results to
-#     # results.md
+#   count_nitrogens.py \
+#     --dirs ./1,./2,./3 \
+#     --complete complete.csv \
+#     --summary results.md
+#   # Reads molecules in directories ./1, ./2, and ./3, and writes number of
+#   # trivalent nitrogens in each molecule to complete.md, and summary
+#   # statistics to results.md
 
 import argparse
 import glob
 import logging
 import sys
 from collections import defaultdict
-from pathlib import Path
 from openeye import oechem
 from tqdm import tqdm
 
@@ -36,7 +43,12 @@ def parse_commandline_flags() -> {str: "argument value"}:
         default="",
         metavar="DIR1,DIR2,DIR3,..",
         help="Comma-separated list of directories of mol2 files")
-    parser.add_argument("--output",
+    parser.add_argument(
+        "--complete",
+        default="./complete.csv",
+        metavar="CSV",
+        help="CSV file with trivalent nitrogens in each molecule")
+    parser.add_argument("--summary",
                         default="./results.md",
                         metavar="MD",
                         help="Output markdown file")
@@ -68,21 +80,26 @@ def configure_logging(loglevel: str):
                         level=numeric_level)
 
 
-def num_nitrogens_in_molecule(mol2file):
+def num_nitrogens_in_molecule(mol: oechem.OEMol):
     """
-    Counts the number of trivalent nitrogens in the molecule in the given file.
+    Counts the number of trivalent nitrogens in the given molecule.
     """
-    istream = oechem.oemolistream(mol2file)
-    istream.SetFormat(oechem.OEFormat_MOL2)
-    mol = oechem.OEMol()
-    oechem.OEReadMolecule(istream, mol)
-    return oechem.OECount(mol, oechem.OEIsInvertibleNitrogen())
+    checker = oechem.OEIsInvertibleNitrogen()
+    num_trivalent_nitrogens = 0
+    for atom in mol.GetAtoms():
+        if checker(atom):
+            num_trivalent_nitrogens += 1
+    return num_trivalent_nitrogens
 
 
-def count_in_directories(dirs: [str]) -> {int: int}:
+def count_in_directories(dirs: [str], complete_csv: "file object") -> {
+        int: int
+}:
     """
     Counts trivalent nitrogens in mol2 molecules in each directory in
     dirs.
+
+    Write data for each molecule in complete_csv.
 
     Returns a dictionary mapping the number of trivalent nitrogens to the number
     of molecules having that number.
@@ -91,17 +108,24 @@ def count_in_directories(dirs: [str]) -> {int: int}:
     for mol2dir in dirs:
         logging.info("Counting nitrogens in molecules in %s", mol2dir)
         for mol2file in tqdm(glob.iglob(mol2dir + "/*.mol2")):
-            n = num_nitrogens_in_molecule(mol2file)
+            istream = oechem.oemolistream(mol2file)
+            istream.SetFormat(oechem.OEFormat_MOL2)
+            mol = oechem.OEMol()
+            oechem.OEReadMolecule(istream, mol)
+
+            n = num_nitrogens_in_molecule(mol)
             counts[n] += 1
+
+            complete_csv.write(f"{mol2file},{oechem.OEMolToSmiles(mol)},{n}\n")
     return counts
 
 
-def save_results(counts, filename):
-    """Write results to the given file."""
+def save_results(counts: {int: int}, filename: str):
+    """Write summary statistics to the given file."""
     logging.info("Saving results to %s", filename)
     f = open(filename, "w")
 
-    # Calculate some statistics
+    # Calculate additional statistics
     mean = sum(num * val for num, val in counts.items()) / sum(counts.values())
     total_molecules = sum(counts.values())
     molecules_with_more_than_0 = total_molecules - counts[0]
@@ -132,8 +156,12 @@ def save_results(counts, filename):
 def main():
     args = parse_commandline_flags()
     configure_logging(args["log"])
-    counts = count_in_directories(args["dirs"])
-    save_results(counts, args["output"])
+
+    complete_csv = open(args["complete"], "w")
+    complete_csv.write("filepath,SMILES,count\n")
+
+    counts = count_in_directories(args["dirs"], complete_csv)
+    save_results(counts, args["summary"])
 
 
 if __name__ == "__main__":
